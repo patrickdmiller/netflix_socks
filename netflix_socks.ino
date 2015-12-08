c6a7e2b85311bbf22753e5fe246c9a573ff38d7b
Patrick Miller <patrick@deeplocal.com>
2015-12-08 12:17:37 -0500

    cleaning up code and adding comments

--- netflix_socks.ino
+++ netflix_socks.ino
@@ -9,43 +9,49 @@
 #define INDICATOR_PIN 8
 #define SOFT_SWITCH_PIN 3
 
-//accelerometers
+//accelerometer
 #define x A3
 #define y A4
 #define z A5
 
 //ir to stop the show (stop button on our Sony NRTV remote)
+//put your timings here
 #define IR_PIN     4
 #define NUM_PULSES 60
 int pulse_widths[NUM_PULSES][2] = {{4716,2416},{572,628},{568,628},{572,616},{580,1224},{572,1224},{568,620},{580,620},{572,1220},{576,1228},{568,1224},{572,616},{580,1224},{572,620},{576,620},{576,1220},{20308,2420},{572,628},{568,620},{576,624},{572,1220},{576,1220},{576,620},{576,624},{572,1220},{576,1220},{572,1220},{576,624},{572,1220},{576,624},{572,620},{576,1224},{20304,2416},{572,620},{576,620},{576,624},{572,1224},{572,1220},{576,624},{572,628},{568,1224},{568,1224},{572,1224},{572,624},{572,1224},{568,628},{572,620},{576,1216},{20360,2420},{568,620},{576,624},{572,628},{568,1224},{572,1224},{572,624},{568,624},{576,1216},{576,1216},{580,1224},{572,620}};
 
 // ======================== VARS ==============================
 Adafruit_NeoPixel indicator = Adafruit_NeoPixel(1, INDICATOR_PIN);
-///color for each state.
 uint8_t colors[3][3] = {{1, 1, 1}, {1, 0, 1}, {1, 0, 0}}; //color multiplier per state, 1,1,1 is white, 1,0,0, is red, etc.
 uint8_t colorState = 255;
 uint8_t colorPulseIncrement = -1;
 volatile bool cpuSleepFlag = true;
-
-int userSleepState = 0; //0 awake, 1 possible asleep, 2 asleep
-bool userReallyAsleep = false;
+/*
+The way this works is we keep a running sum of acceleration magnitude for all three axis. 
+We take this sum over a window of time. If the sum < threshold, then the user will move into userSleepState of 1.
+if the user is in userSleepState1 for consecutive windows for consecutiveThresholdTime, then we move into userSleepState of 2
+we then notify the user with a red light. if the user does not surpase the threshold before the userReallyAsleepDelay is reached, then the user is considered asleep.
+*/
 unsigned long nextReadTime, windowTime, cpuAwoken, userReallyAsleepStart;
-int readDelay = 50; //read accelerometer every 50 ms
-int windowDelay = 1500; //compute displacement every window
+int readDelay = 50; //read accelerometer every x ms
+int windowDelay = 1500; //compute displacement every window of time
 int userReallyAsleepDelay = 1 * 60 * 1000; // 1 minute
-int consecutivePossibleSleeps = 0;
-int threshold = 50; //amount of movement that sleep is less than. this is a sum magnitude so the lower the window, the lower this should be. This can be converted to a rate by making it a fn of window time.
-int thresholdTime = 30; // in seconds
-bool newWindow = true;
+int threshold = 50; //amount of movement that sleep is less than. this is a sum magnitude so the lower the window, the lower this should be. This can be converted to fn of window time.
+int consecutiveThresholdTime = 30; // in seconds
+int userSleepState = 0; //0 awake, 1 possible asleep, 2 asleep
+bool userReallyAsleep = false; //flag is set after sleep is 'confirmed'
+bool newWindow = true; //when a new window is hit
 //accelerometer values
 int pxVal = 0;
 int pyVal = 0;
 int pzVal = 0;
-int movementDisplacement = 0;
+int movementSum = 0; //running total of accelerometer magnitude
+int consecutivePossibleSleeps = 0; //consecutive windows where possibly asleep.
 
 // ======================== SETUP ==============================
 void setup() {
-  analogReference(EXTERNAL); // use AREF for reference voltage
+  analogReference(EXTERNAL); //aref reference voltage
+  //setup pins
   pinMode(SOFT_SWITCH_PIN, INPUT);
   pinMode(IR_PIN, OUTPUT);
   digitalWrite(IR_PIN, LOW);
@@ -54,29 +60,30 @@ void setup() {
   windowTime = nextReadTime;
   //indicator light
   indicator.begin();
-  indicator.setBrightness(100); 
-  //start asleep
+  indicator.setBrightness(100); //pick a good brightness 
+  //start with cpu sleep
   cpuSleepNow();
-
 }
 
 // ======================== LOOP ==============================
-
 void loop() {
+  
+  //everthing broken up into seperate functions to keep things simple.
   indicatorHandler();
   softSwitchHandler();
   sleepHandler();
   irHandler();
-  //if a window time is up, make it a new window for a sliding window average
+  
+  //new calculation window every windowDelay
   if (millis() - windowTime > windowDelay) {
     windowTime = millis();
     newWindow = true;
   }
   
-  //if we've reached the readDelay, read accelerometer
+  //read accelerometer every readDelay amount of time
   if (millis() - nextReadTime > readDelay) {
     nextReadTime = millis();
-   accelerometerHandler();
+    accelerometerHandler();
   }
 }
 
@@ -95,10 +102,10 @@ void accelerometerHandler() {
   int yVal = analogRead(y);
   int zVal = analogRead(z);
   
-  //if its a new iwn
+  //if its a new calculation window
   if (newWindow) {
-    //just set the values to what they currently are
-    if (movementDisplacement < threshold) {
+    //if the displacement is less than the threshold, then the user may be asleep.
+    if (movementSum < threshold) {
       if(userSleepState !=2){
         userSleepState = 1;  
       }
@@ -106,25 +113,23 @@ void accelerometerHandler() {
       userSleepState = 0;
     }
 
-
     if (userSleepState >= 1) {
       consecutivePossibleSleeps++;
-      if (consecutivePossibleSleeps > (thresholdTime * ((float)1000/windowDelay))){//((1000 / windowDelay) * 60 * thresholdTime)) {
+      if (consecutivePossibleSleeps > (consecutiveThresholdTime * ((float)1000/windowDelay))){
         userSleepState = 2;
-      } //120 per minute
+      }
     } else {
-//      userSleepState = 1;
       consecutivePossibleSleeps = 0;
     }
 
     pxVal = xVal;
     pyVal = yVal;
     pzVal = zVal;
-    movementDisplacement = 0;
+    movementSum = 0;
     newWindow = false;
   } else {
     //compute magnitude changes
-    movementDisplacement += abs(xVal - pxVal) + abs(yVal - pyVal) + abs(zVal - pzVal);
+    movementSum += abs(xVal - pxVal) + abs(yVal - pyVal) + abs(zVal - pzVal);
   }
 }
 // ======================== USER SLEEP ==============================
@@ -139,9 +144,8 @@ void sleepHandler(){
 }
 
 // ======================== INDICATOR ==============================
-
 void indicatorHandler(){
-  //pulse
+  //pulse the indicator for whichever color it is. can change what the pulse looks like here. 
   colorState+=colorPulseIncrement;
   if(colorState < 1){
     colorPulseIncrement = 0;
@@ -154,16 +158,15 @@ void indicatorHandler(){
 }
 
 // ======================== CPU SLEEP ==============================
-
 void cpuSleepNow() {  
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here  
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN); 
     sleep_enable();
     delay(100);
+    //turn stuff off
     indicator.setPixelColor(0,0,0,0);
     indicator.show();
     attachInterrupt(digitalPinToInterrupt(3),pinInterrupt, FALLING);
     sleep_mode();
-    // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP  
     sleep_disable();
     detachInterrupt(digitalPinToInterrupt(3));
 }  
@@ -185,19 +188,18 @@ void reSetup(){
 // ======================== IR ==============================
 void irHandler(){
   if(userReallyAsleep){
-    //shut down the indicator since we will be pulling a lot of power on the 3.3
+    //shut down the indicator
     indicator.setPixelColor(0,0,0,0);
     indicator.show();
     IR_transmit_pwr();
-    //repeat if desired.
+    //repeat if desired. or add any other codes syou may want to handle. This is whatever your TV needs.
     delay(500);
     IR_transmit_pwr();
     //put the microcontrolelr to sleep
     cpuSleepNow();
-    
   }
 }
-
+//based on src from https://learn.adafruit.com/ir-sensor
 void pulseIR(long microsecs) {
   while (microsecs > 0) {
     // 38 kHz is about 13 microseconds high and 13 microseconds low
